a6f1f80c24a36256d54a34fcf8fa2fda903248f0
Bunna Veth <bveth@xactware.com>
2020-09-11 07:32:39 -0600

    Initial Commit

---
+++ TokboxARSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TokboxARSample
+//
+//  Created by Bunna Veth on 9/11/20.
+//  Copyright © 2020 Xactware. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
